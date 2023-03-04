a16c043529b324aa1b928ae164f0e73e0511f760
James Ng <70497056+james-ng4321@users.noreply.github.com>
2023-03-04 22:20:58 +0800

    Add files via upload

---
+++ galaxy 2.user.js
@@ -0,0 +1,20 @@
+// ==UserScript==
+// @name         galaxy 2
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://ticketing.galaxymacau.com/content/galaxy/home.aspx
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=livenation.hk
+// @grant        none
+// ==/UserScript==
+
+
+
+(function() {
+    'use strict';
+    //location.replace("https://www.livenation.hk/show/1395392/blackpink-world-tour-born-pink-hong-kong/hong%20kong/2023-01-15/en?_gl=1*wi3any*_up*MQ..*_ga*MTY2OTQ1MDI1Mi4xNjY5MDE3MzIx*_ga_Y19VVP0K58*MTY2OTAxNzMyMC4xLjEuMTY2OTAxNzQ1OS4wLjAuMA..")
+//    location.replace("https://www.livenation.hk/en/artist-blackpink-1239361?_gl=1*c8oygb*_up*MQ..*_ga*NDAyNjkxNzIyLjE2NjkwMTc4MDU.*_ga_Y19VVP0K58*MTY2OTAxNzgwNS4xLjAuMTY2OTAxNzgwNS4wLjAuMA..")
+    location.replace("https://ticketing.galaxymacau.com/shows/show.aspx?sh=BPBPWT23")
+    // Your code here...
+})();
